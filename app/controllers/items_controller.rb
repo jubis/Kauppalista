1c42705e5f5b4be367f21a28a6ad13f02c1b4ef9
Matias Saarinen <matias.saarinen@aalto.fi>
2013-03-05 20:46:45 +0200

    user can remove and add items to own lists

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_filter :auth_user_or_render_error
+  before_filter :list_belongs_to_current_user, :only => ( 'create', 'destroy' )
 
   def new
   	@item = Item.new
@@ -10,26 +11,30 @@ class ItemsController < ApplicationController
   def create
   	@item = Item.new( params[ :item ] )
 
-    valid_list_id = list_belongs_to_current_user
-  	if valid_list_id && @item.save
+  	if @valid_user && @item.save
   		render :text => 'saved succesfully'
   	else
-      invalid_list_id = (!valid_list_id ? 'invalid list_id ' + @item.list_id.to_s + ' ' : '')
+      invalid_list_id = (!@valid_user ? 'invalid list_id ' + @item.list_id.to_s + ' ' : '')
   		render :text => 'saving failed ' +  invalid_list_id + params[ :item ].to_s
   	end
   end
 
   def destroy
-  	Item.find( params[ :id ] ).destroy
-  	render :text => 'deleted'
+    if @valid_user
+      Item.find( params[ :id ] ).destroy
+      render :text => 'destroyed'
+    else
+      render :text => 'not authorized user for destoying'
+    end
   end
 
   def list_belongs_to_current_user
     for list in @current_user.lists
       if @item.list_id == list.id
-        return true
+        @valid_user = true
+        return
       end
     end
-    return false
+    @valid_user = false
   end
 end
