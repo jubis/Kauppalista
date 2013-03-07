eafd2bcbd0c07b0431462464edddf5c557c1066d
Matias Saarinen <matias.saarinen@aalto.fi>
2013-03-07 22:07:49 +0200

    bugfix: checking if item belongs to current user modified

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   before_filter :auth_user_or_render_error
-  before_filter :list_belongs_to_current_user, :only => [ 'create', 'destroy' ]
 
   def new
   	@item = Item.new
@@ -9,32 +8,34 @@ class ItemsController < ApplicationController
   end
 
   def create
-  	@item = Item.new( params[ :item ] )
+  	item = Item.new( params[ :item ] )
 
-  	if @valid_user && @item.save
+  	if list_belongs_to_current_user( item ) && item.save
   		render :text => 'saved succesfully'
   	else
-      invalid_list_id = (!@valid_user ? 'invalid list_id ' + @item.list_id.to_s + ' ' : '')
+      invalid_list_id = (!@valid_user ? 'invalid list_id ' + item.list_id.to_s + ' ' : '')
   		render :text => 'saving failed ' +  invalid_list_id + params[ :item ].to_s
   	end
   end
 
   def destroy
-    if @valid_user
-      Item.find( params[ :id ] ).destroy
+    item = Item.find( params[ :id ] )
+    if list_belongs_to_current_user( item )
+      item.destroy
       render :text => 'destroyed'
     else
       render :text => 'not authorized user for destoying'
     end
   end
 
-  def list_belongs_to_current_user
+  def list_belongs_to_current_user( item )
     for list in @current_user.lists
-      if @item.list_id == list.id
+      if item.list_id == list.id
         @valid_user = true
-        return
+        return true
       end
     end
     @valid_user = false
+    return false
   end
 end
